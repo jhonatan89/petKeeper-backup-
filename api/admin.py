4b8e1e8cd4ecd7af7a05ebb7d1acc18a537cdddc
Andrés Felipe Esguerra Restrepo <kaosterra@gmail.com>
2016-04-04 21:58:01 -0500

    Registro de modelo en admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Breed, Size, Offer, Pet, Request
 # Register your models here.
+
+admin.site.register(Breed)
+admin.site.register(Size)
+admin.site.register(Offer)
+admin.site.register(Pet)
+admin.site.register(Request)
